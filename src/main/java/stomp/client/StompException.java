e3a5791703573e1380755e994b3d2bc567ef14ef
dewmal <dewmalnilanka@gmail.com>
2013-10-23 23:35:44 +0530

    convert project to maven project

---
+++ src/main/java/stomp/client/StompException.java
@@ -0,0 +1,16 @@
+package stomp.client;
+
+public class StompException extends Exception {
+	private static final long serialVersionUID = 5475019401678519895L;
+	private String message;
+
+	public StompException(String message) {
+		super(message);
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ src/main/java/stomp/client/test/async/test.java
@@ -0,0 +1,77 @@
+package stomp.client.test.async;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import stomp.client.Ack;
+import stomp.client.StompClient;
+import stomp.client.StompException;
+
+public class test {
+
+	public static void main(String[] args) throws StompException, URISyntaxException {
+		new test().run(new URI("tcp://login:passcode@localhost:61613"));
+	}
+	
+	public void run(URI uri) throws StompException {
+		
+		StompClient client = new StompClient(uri) {
+			public void onConnected(String sessionId) {
+				System.out.println("connected: sessionId = " + sessionId);
+			}
+			
+			public void onDisconnected() {
+				System.out.println("disconnected");
+			}
+			
+			public void onMessage(String messageId, String body) {
+				System.out.println("message: messageId = " + messageId + " body = " + body);
+				try {
+					ack(messageId);
+				} catch (StompException e) {
+				}
+			}
+			
+			public void onReceipt(String receiptId) {
+				System.out.println("receipt: receiptId = " + receiptId);
+			}
+			
+			public void onError(String message, String description) {
+				System.out.println("error: message = " + message + " description = " + description);
+			}
+
+			public void onCriticalError(Exception e) {
+				e.printStackTrace();
+			}
+		};
+		
+		// connect to STOMP server, send CONNECT command and wait CONNECTED answer
+		client.connect();
+		
+		// subscribe on queue
+		client.subscribe("/queue/test", Ack.client);
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e1) {
+		}
+		
+		
+		// send 100 messages
+		for(int i=0; i<100; i++) {
+			client.send("/queue/test", "message #" + i);
+		}
+		
+		// wait		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		
+		// unsubscribe
+		client.unsubscribe("/queue/test");
+		
+		// disconnect
+		client.disconnect();
+		
+	}
+
+}
